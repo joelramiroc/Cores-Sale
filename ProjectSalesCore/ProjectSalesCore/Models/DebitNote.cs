9b4aa11fa6d2fc253f414f84d83f13a0d08bac5f
Pablo Lopez <pablolopezq@unitec.edu>
2018-06-04 11:00:36 -0600

    models

---
+++ ProjectSalesCore/ProjectSalesCore/Models/DebitNote.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="DebitNote.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CSales.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [Table("DNOTE")]
+
+    public class DebitNote
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("ID")]
+        public long IdDebitNote { get; set; }
+
+        [ForeignKey(nameof(Client))]
+        [Column("IDCLIENT")]
+        public long IdClient { get; set; }
+
+        [Column("CLIENT")]
+        public virtual Client Client { get; set; }
+
+        [ForeignKey(nameof(Employee))]
+        [Column("IDEMPLOYEE")]
+        public long IdEmployee { get; set; }
+
+        [Column("EMPLOYEE")]
+        public virtual Employee Employee { get; set; }
+
+        [ForeignKey(nameof(TypeOfSaleDocument))]
+        [Column("IDDOCFORSALE")]
+        public long IdTypeDocumentForSale { get; set; }
+
+        [Column("IDTDFORSALE")]
+        public virtual TypeOfSaleDocument TypeOfSaleDocument { get; set; }
+
+        [Column("IDDOCUMENT")]
+        public long IdDocument { get; set; }
+
+        [Column("TOTAL")]
+        public decimal Total { get; set; }
+    }
+}
---
+++ ProjectSalesCore/ProjectSalesCore/Models/ExternalProduct.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="ExternalProduct.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CSales.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [Table("EPRODUCT")]
+
+    public class EProduct
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("ID")]
+        public long IdExternalProduct { get; set; }
+
+        [ForeignKey(nameof(Product))]
+        [Column("IDPRODUCT")]
+        public long IdProduct { get; set; }
+
+        [Column("PRODUCT")]
+        public virtual Product Product { get; set; }
+
+        [ForeignKey(nameof(UnitOfMeasurement))]
+        [Column("IDM")]
+        public long IdUnitOfMeasurement { get; set; }
+
+        [Column("UNITOFMEASUREMENT")]
+        public virtual UnitOfMeasurement UnitOfMeasurement { get; set; }
+
+        [ForeignKey(nameof(ProductType))]
+        [Column("IDPT")]
+        public long IdProductType { get; set; }
+
+        [Column("PRODUCTTYPE")]
+        public virtual ProductType ProductType { get; set; }
+
+        [ForeignKey(nameof(ProductLine))]
+        [Column("IDPL")]
+        public long IdProductLine { get; set; }
+
+        [Column("PRODUCTLINE")]
+        public virtual ProductLine ProductLine { get; set; }
+
+        [Column("PRODUCTDESCRIPTION")]
+        public string ProductDescription { get; set; }
+
+        [Column("QUANTITY")]
+        public int Quantity { get; set; }
+
+        [Column("ACTIVE")]
+        public bool Active { get; set; }
+    }
+}
